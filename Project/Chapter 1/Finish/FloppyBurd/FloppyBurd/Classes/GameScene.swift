4cf62b50a52d129ca6a2a5fc2058e802e67d8808
Jeremy Novak <jgnovak@gmail.com>
2016-02-22 21:20:15 -0600

    Project Organization
    Project Organization

---
+++ Project/Chapter 1/Finish/FloppyBurd/FloppyBurd/Classes/GameScene.swift
@@ -0,0 +1,61 @@
+//
+//  GameScene.swift
+//  floppyburd
+//
+//  Created by Jeremy Novak on 2/20/16.
+//  Copyright (c) 2016 Jeremy Novak. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene:SKScene {
+    
+    // MARK: - Private class constants
+    private let tutorialButton = TutorialButton()
+    
+    // MARK: - Init
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+    }
+    
+    override func didMoveToView(view: SKView) {
+        self.setupMenuScene()
+    }
+    
+    // MARK: - Setup
+    private func setupMenuScene() {
+        // Set the background color
+        self.backgroundColor = Colors.colorFromRGB(rgbvalue: Colors.Background)
+        
+        // Add the play button
+        self.addChild(self.tutorialButton)
+    }
+    
+    // MARK: - Update
+    override func update(currentTime: NSTimeInterval) {
+    }
+    
+    // MARK: - Touch Events
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch:UITouch = touches.first! as UITouch
+        let touchLocation = touch.locationInNode(self)
+        
+        if self.tutorialButton.containsPoint(touchLocation) {
+            self.tutorialButton.tapped()
+            
+            self.loadGameOverScene()
+        }
+    }
+    
+    // MARK: - Load Scene
+    private func loadGameOverScene() {
+        let gameOverScene = GameOverScene(size: kViewSize)
+        let transition = SKTransition.fadeWithColor(SKColor.blackColor(), duration: 0.25)
+        
+        self.view?.presentScene(gameOverScene, transition: transition)
+    }
+}

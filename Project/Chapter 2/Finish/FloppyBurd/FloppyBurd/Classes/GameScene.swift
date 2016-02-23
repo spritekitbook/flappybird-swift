8d9c04fbd959979fa5b55d7b43dfecf4e6ce226a
Jeremy Novak <jgnovak@gmail.com>
2016-02-23 09:27:12 -0600

    Project Organization
    Project Organization

---
+++ Project/Chapter 2/Finish/FloppyBurd/FloppyBurd/Classes/GameScene.swift
@@ -0,0 +1,86 @@
+//
+//  GameScene.swift
+//  floppyburd
+//
+//  Created by Jeremy Novak on 2/20/16.
+//  Copyright (c) 2016 Jeremy Novak. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene:SKScene, SKPhysicsContactDelegate {
+    
+    // MARK: - Private class constants
+    private let tutorialButton = TutorialButton()
+    private let gameNode = SKNode()
+    private let ground = Ground()
+    
+    // MARK: - Private class variables
+    private var lastUpdateTime:NSTimeInterval = 0.0
+    
+    // MARK: - Init
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(size: CGSize) {
+        super.init(size: size)
+    }
+    
+    override func didMoveToView(view: SKView) {
+        self.setupMenuScene()
+    }
+    
+    // MARK: - Setup
+    private func setupMenuScene() {
+        // Set the background color
+        self.backgroundColor = Colors.colorFromRGB(rgbvalue: Colors.Background)
+        
+        self.physicsWorld.contactDelegate = self
+        self.physicsWorld.gravity = CGVectorMake(0, 0)
+        
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.physicsBody?.dynamic = false
+        self.physicsBody?.affectedByGravity = false
+        self.physicsBody?.categoryBitMask = Contact.Scene
+        self.physicsBody?.collisionBitMask = 0x0
+        self.physicsBody?.contactTestBitMask = 0x0
+        
+        // Add the play button
+        self.addChild(self.tutorialButton)
+    }
+    
+    // MARK: - Update
+    override func update(currentTime: NSTimeInterval) {
+    }
+    
+    // MARK: - Touch Events
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch:UITouch = touches.first! as UITouch
+        let touchLocation = touch.locationInNode(self)
+        
+        if self.tutorialButton.containsPoint(touchLocation) {
+            self.tutorialButton.tapped()
+            
+            //self.loadGameOverScene()
+            self.enableGravity()
+        }
+    }
+    
+    // MARK: - Temporary function
+    private func enableGravity() {
+        self.physicsWorld.gravity = CGVectorMake(0, -10)
+        
+        self.tutorialButton.runAction(SKAction.waitForDuration(0.25), completion: {
+            self.tutorialButton.removeFromParent()
+        })
+    }
+    
+    // MARK: - Load Scene
+    private func loadGameOverScene() {
+        let gameOverScene = GameOverScene(size: kViewSize)
+        let transition = SKTransition.fadeWithColor(SKColor.blackColor(), duration: 0.25)
+        
+        self.view?.presentScene(gameOverScene, transition: transition)
+    }
+}
